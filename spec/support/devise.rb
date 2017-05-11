8cff02a1448786a8b78664d7f442af40b4cc260c
Winkeltje84 <bwinkel@gmail.com>
2017-05-11 11:29:41 +0200

    created factory girl rb file & configuration of devise /  anabled autoloading

---
+++ spec/support/devise.rb
@@ -0,0 +1,5 @@
+RSpec.configur do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
+end
