5f64fef26e97b0a5d22927a826bc1c9509ddf548
Winkeltje84 <bwinkel@gmail.com>
2017-05-12 14:33:15 +0200

    fixed odometer error - lenght had to be max length six

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Bike.destroy_all
 User.destroy_all
 
-basTest = User.create(email: "bas@test.com", password: "abc123")
+basTest = User.create!(email: "bas@test.com", password: "abc123")
 
-bike1 = Bike.create(name: "Mr Orange", brand: "Suzuki", model: "DL650", build: 2010, odometer: 71000, user: basTest)
+bike1 = Bike.create!(name: "Mr Orange", brand: "Suzuki", model: "DL650", build: 2010, odometer: 71000, user: basTest)
