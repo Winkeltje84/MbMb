e62a589e27929096299afc58bc6d49ca107374f8
Winkeltje84 <bwinkel@gmail.com>
2017-05-12 14:38:18 +0200

    added 2nd bike

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ User.destroy_all
 basTest = User.create!(email: "bas@test.com", password: "abc123")
 
 bike1 = Bike.create!(name: "Mr Orange", brand: "Suzuki", model: "DL650", build: 2010, odometer: 71000, user: basTest)
+bike2 = Bike.create!(name: "WantToHave", brand: "Honda", model: "CRF1000L", build: 2017, odometer: 0, user: basTest)
