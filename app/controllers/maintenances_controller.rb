6f86f359d04f1bcd0305ee29ed75457254a76765
Winkeltje84 <bwinkel@gmail.com>
2017-05-17 16:44:52 +0200

    changed some scss

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -1,5 +1,6 @@
 class MaintenancesController < ApplicationController
   before_action :set_maintenance, only: [:edit, :update]
+  before_action :set_bike, only: [:index]
   before_action :authenticate_user!
 
   def index
@@ -10,5 +11,9 @@ class MaintenancesController < ApplicationController
 
     def set_maintenance
       @maintenance = Maintenance.find(params[:id])
+    end
+
+    def set_bike
+      @bike = Bike.find(params[:bike_id])
     end
 end
