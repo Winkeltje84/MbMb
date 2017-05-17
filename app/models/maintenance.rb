8e4902810d78e65ef22c86550994072f97fe2ec1
Winkeltje84 <bwinkel@gmail.com>
2017-05-17 13:31:47 +0200

    got rid of validations since it didnt seem to work

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,20 +2,15 @@ class Maintenance < ApplicationRecord
   belongs_to :bike
 
   validates :odometer, presence: true, length: { maximum: 6 }
-  validates :date, presence: true,
-            format: { with: /(19|20)\d{2}/i },
-            numericality: {
-              greater_than_or_equal_to: 100.years.ago,
-              less_than_or_equal_to: Date.today
-            }
-  validates :oil, presence: true
-  validates :sparkplugs, presence: true
-  validates :airfilter, presence: true
-  validates :oilfilter, presence: true
-  validates :coolingfluid, presence: true
-  validates :chain, presence: true
-  validates :break_front, presence: true
-  validates :break_front, presence: true
-  validates :breakfluid, presence: true
-  validates :tyres, presence: true
+  validates :date, presence: true, format: { with: /(19|20)\d{2}/i }
+  # validates :oil, presence: true, inclusion: { in: [true, false] }
+  # validates :sparkplugs, presence: true, inclusion: { in: [true, false] }
+  # validates :airfilter, presence: true
+  # validates :oilfilter, presence: true
+  # validates :coolingfluid, presence: true
+  # validates :chain, presence: true
+  # validates :break_front, presence: true
+  # validates :break_back, presence: true
+  # validates :breakfluid, presence: true
+  # validates :tyres, presence: true
 end
