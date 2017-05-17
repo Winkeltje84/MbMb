3222d9eeb92dc17ebc31f67db756a1527272fa8a
Winkeltje84 <bwinkel@gmail.com>
2017-05-17 15:40:58 +0200

    changed  maintenance factory

--- spec/factories/maintenances.rb
+++ spec/factories/maintenances.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :maintenance do
     odometer 1
-    date "2017-05-16 18:05:32"
-    costs "9.99"
-    oil false
-    sparkplugs false
-    airfilter false
-    oilfilter false
-    coolingfluid false
-    chain false
-    break_front false
-    break_back false
-    breakfluid false
-    tyres false
-    bike nil
+    date "2017-05-16"
+    costs Faker::Number.decimal(2)
+    oil Faker::Boolean.boolean
+    sparkplugs Faker::Boolean.boolean
+    airfilter Faker::Boolean.boolean
+    oilfilter Faker::Boolean.boolean
+    coolingfluid Faker::Boolean.boolean
+    chain Faker::Boolean.boolean
+    break_front Faker::Boolean.boolean
+    break_back Faker::Boolean.boolean
+    breakfluid Faker::Boolean.boolean
+    tyres Faker::Boolean.boolean
+    bike { build(:bike) }
   end
 end
