d580b99af55128451c5a90e6a765200ba3a5b011
Winkeltje84 <bwinkel@gmail.com>
2017-05-19 15:48:16 +0200

    fixed validations, now checks if true or false is present

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -3,16 +3,16 @@ class Maintenance < ApplicationRecord
 
   validates :odometer, presence: true, length: { maximum: 6 }
   validates :date, presence: true , format: { with: /(19|20)\d{2}/i }
-  # validates :oil, presence: true, inclusion: { in: [true, false] }
-  # validates :sparkplugs, presence: true, inclusion: { in: [true, false] }
-  # validates :airfilter, presence: true
-  # validates :oilfilter, presence: true
-  # validates :coolingfluid, presence: true
-  # validates :chain, presence: true
-  # validates :break_front, presence: true
-  # validates :break_back, presence: true
-  # validates :breakfluid, presence: true
-  # validates :tyres, presence: true
+  validates :oil, inclusion: [true, false]
+  validates :sparkplugs, inclusion: [true, false]
+  validates :airfilter, inclusion: [true, false]
+  validates :oilfilter, inclusion: [true, false]
+  validates :coolingfluid, inclusion: [true, false]
+  validates :chain, inclusion: [true, false]
+  validates :break_front, inclusion: [true, false]
+  validates :break_back, inclusion: [true, false]
+  validates :breakfluid, inclusion: [true, false]
+  validates :tyres, inclusion: [true, false]
 
   def self.find_bike_maintenances(bike_id)
     bike_maintenances = Maintenance.select{|maintenance| maintenance.bike_id == bike_id}
