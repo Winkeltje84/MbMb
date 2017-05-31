d3495d7df87eaaa6d679225c4140d967fdb03cfb
Winkeltje84 <bwinkel@gmail.com>
2017-05-31 16:01:10 +0200

    added view for maintenance Schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,14 +1,19 @@
 class SchedulesController < ApplicationController
   before_action :set_schedule, only: [:show]
+  before_action :set_bike_name, only: [:show]
 
   def show
-    debugger
+    # debugger
   end
 
   private
 
     def set_schedule
-      debugger
+      # debugger
       @schedule = Schedule.find(params[:id])
+    end
+
+    def set_bike_name
+      @bike_name = Bike.find(params[:bike_id]).name
     end
 end
