834e9684f904790115ff692de89ee3969edba5f6
Winkeltje84 <bwinkel@gmail.com>
2017-05-31 14:09:19 +0200

    added date sorting method for index

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -15,7 +15,16 @@ class Maintenance < ApplicationRecord
   validates :tyres, inclusion: [true, false]
 
   def self.find_bike_maintenances(bike_id)
-    bike_maintenances = Maintenance.select{|maintenance| maintenance.bike_id == bike_id}
+    bike_maintenances = Maintenance.select{ |maintenance| maintenance.bike_id == bike_id }
+    sort_by_date(bike_maintenances)
+    # bike_maintenances.sort_by_date
     return bike_maintenances
   end
+
+  def self.sort_by_date(bike_maintenances)
+    bike_maintenances.sort! { |a,b| a.date <=> b.date } 
+    return bike_maintenances
+  end
+
+
 end
