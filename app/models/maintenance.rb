7319e730e93d0d95e3a8f61cc3f2e42cdfc93d22
Winkeltje84 <bwinkel@gmail.com>
2017-06-06 23:03:03 +0200

    created calculation functionality, still in need of serious refactor

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -16,39 +16,73 @@ class Maintenance < ApplicationRecord
 
   def self.find_bike_maintenances(bike_id)
     bike_maintenances = Maintenance.select{ |maintenance| maintenance.bike_id == bike_id }
-    # sort_by_date(bike_maintenances)
-    # bike_maintenances.sort_by_date
     return bike_maintenances
   end
 
-  def self.sort_by_date(bike_maintenances)
+  def self.sort_by_date_from_old_to_current(bike_maintenances)
     bike_maintenances.sort! { |a,b| a.date <=> b.date }
     return bike_maintenances
   end
 
   def self.calculate_km(bike_maintenances, current_odometer)
-    # debugger
-    # bike_maintenances.sort_by_date_from_current_to_old
     sort_by_date_from_current_to_old(bike_maintenances)
-    kilometers = {} # create new object that will receive all km
-    # debugger
-    find_oil(bike_maintenances, kilometers, current_odometer)
+    kilometers = set_km_to_no_maintenance(current_odometer) #kilometers = {} # create new object that will receive all km
+    find_last_item_maintenance(bike_maintenances, kilometers, current_odometer)
     return kilometers
   end
 
   def self.sort_by_date_from_current_to_old(bike_maintenances)
     bike_maintenances.sort! { |a,b| b.date <=> a.date }
-    # return bike_maintenances
   end
 
-  def self.find_oil(bike_maintenances, kilometers, current_odometer)
+  def self.find_last_item_maintenance(bike_maintenances, kilometers, current_odometer)
     bike_maintenances.each do |maintenance|
-
-      if (maintenance.oil === true)
+      if ((kilometers[:oil] === current_odometer) && (maintenance.oil === true))
         kilometers[:oil] = (current_odometer - maintenance.odometer)
       end
-      # debugger
+      if ((kilometers[:sparkplugs] === current_odometer) && (maintenance.sparkplugs === true))
+        kilometers[:sparkplugs] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:airfilter] === current_odometer) && (maintenance.airfilter === true))
+        kilometers[:airfilter] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:oilfilter] === current_odometer) && (maintenance.oilfilter === true))
+        kilometers[:oilfilter] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:coolingfluid] === current_odometer) && (maintenance.coolingfluid === true))
+        kilometers[:coolingfluid] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:chain] === current_odometer) && (maintenance.chain === true))
+        kilometers[:chain] = (current_odometer - maintenance.odometer)
+      end
+    if ((kilometers[:break_front] === current_odometer) && (maintenance.break_front === true))
+        kilometers[:break_front] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:break_back] === current_odometer) && (maintenance.break_back === true))
+        kilometers[:break_back] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:breakfluid] === current_odometer) && (maintenance.breakfluid === true))
+        kilometers[:breakfluid] = (current_odometer - maintenance.odometer)
+      end
+      if ((kilometers[:tyres] === current_odometer) && (maintenance.tyres === true))
+        kilometers[:tyres] = (current_odometer - maintenance.odometer)
+      end
     end
+  end
+
+  def self.set_km_to_no_maintenance(current_odometer)
+    return kilometers = {
+      oil: current_odometer,
+      sparkplugs: current_odometer,
+      airfilter: current_odometer,
+      oilfilter: current_odometer,
+      coolingfluid: current_odometer,
+      chain: current_odometer,
+      break_front: current_odometer,
+      break_back: current_odometer,
+      breakfluid: current_odometer,
+      tyres: current_odometer,
+    }
   end
 
 end
